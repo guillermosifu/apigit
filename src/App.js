519bf6c3e0cd230394ad3212e9b3d436515427bf
guillermosifu <sifuentes1201@gmail.com>
2022-09-07 16:05:52 -0500

    proot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+const [search,setSearch] = useState("");
+
+const[user,setUser]=useState({});
+
+const fetchUser = async()=>{
+  try{
+    const response = await fetch (`https://api.github.com/users/${search}`)
+    const data = await response.json();
+
+    setUser(data)
+  } catch(error){
+    console.log("error",error.message)
+  }
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+       <div className='container'>
+        <h1>Buscador de Usuarios</h1>
+        <div className='grid'>
+          <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder='ingresa el nombre de usuario' />          
+        </div>
+        <div>
+          <button onClick={fetchUser}>Buscar</button>
+        </div>
+        <article>
+          <div className='congtainer'>
+            <img src={user.avatar_url} alt="avatar"  className='b-5'/>
+            <h4>Username {user.login}</h4>
+            <p>{user.bio}</p>
+
+          </div>
+        </article>
+
+       </div>
+     
     </div>
   );
 }
